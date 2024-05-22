f03eebfe65af2a8ab3007b394559bb8ca0fce6c3
Bart Joosten <Bartcardi@users.noreply.github.com>
2024-05-22 09:49:29 +0200

    Updates docstrings and adds comments to functions in ETL.py

--- src/ETL.py
+++ src/ETL.py
@@ -51,21 +51,49 @@ def correct_triple_digit_days_dates(df, column_name):
 
 
 def map_source1(df: DataFrame):
-    df = (
-        df.withColumnRenamed("DataSource", "SourceSystem")
-        .withColumn("ClientNumber", col("ClientNumber").cast(IntegerType()))
-        .withColumn("OriginalAmount", col("ClientAmount").cast(DoubleType()))
-        .drop("ClientAmount")
-        .withColumnRenamed("Currency", "OriginalCurrency")
-        .withColumn("NumberOfEmployees", col("NumberOfEmployees").cast(IntegerType()))
-        .withColumn("OnboardingDate", col("ClientSince").cast(DateType()))
-        .drop("ClientSince")
-        .withColumn("LoanCheck", lit(""))
-        # .withColumn("LoanCheck", lit(None).cast(StringType()))
-        .withColumnRenamed("Location", "Country")
-        .withColumn("DiscountCheck", substring(col("EligibleForDiscount"), 1, 1))
-        .drop("EligibleForDiscount")
-        .withColumn("SnapshotDate", col("SnapshotDate").cast(DateType()))
+    """
+    Maps and transforms columns from Source1 data to a standardized schema.
+
+    This function performs the following transformations:
+    * Renames columns to match a standard schema.
+    * Casts relevant columns to their correct data types (Integer, Double, Date).
+    * Extracts the first character of 'EligibleForDiscount' into 'DiscountCheck'.
+    * Creates a new empty column 'LoanCheck'.
+    * Selects and reorders the columns to match the final schema.
+
+    Args:
+        df: A Spark DataFrame representing data from Source1.
+
+    Returns:
+        A transformed Spark DataFrame with a standardized schema.
+    """
+
+    return (
+        df.withColumnRenamed("DataSource", "SourceSystem")  # Rename column
+        .withColumn(
+            "ClientNumber", col("ClientNumber").cast(IntegerType())
+        )  # Cast to Integer
+        .withColumn(
+            "OriginalAmount", col("ClientAmount").cast(DoubleType())
+        )  # Cast to Double
+        .drop("ClientAmount")  # Drop original column after conversion
+        .withColumnRenamed("Currency", "OriginalCurrency")  # Rename column
+        .withColumn(
+            "NumberOfEmployees", col("NumberOfEmployees").cast(IntegerType())
+        )  # Cast to Integer
+        .withColumn(
+            "OnboardingDate", col("ClientSince").cast(DateType())
+        )  # Cast to Date
+        .drop("ClientSince")  # Drop original column after conversion
+        .withColumn("LoanCheck", lit(""))  # Create an empty 'LoanCheck' column
+        .withColumnRenamed("Location", "Country")  # Rename column
+        .withColumn(
+            "DiscountCheck", substring(col("EligibleForDiscount"), 1, 1)
+        )  # Extract first character
+        .drop("EligibleForDiscount")  # Drop original column after extraction
+        .withColumn(
+            "SnapshotDate", col("SnapshotDate").cast(DateType())
+        )  # Cast to Date
     ).select(
         "SourceSystem",
         "ClientGroup",
@@ -78,25 +106,49 @@ def map_source1(df: DataFrame):
         "Country",
         "DiscountCheck",
         "SnapshotDate",
-    )
-    return df
+    )  # Select and reorder columns
 
 
 def map_source2(df: DataFrame):
-    df = (
-        df.withColumn("ClientNumber", col("ClientNumber").cast(IntegerType()))
-        .withColumn("OriginalAmount", col("ClientAmount").cast(DoubleType()))
-        .drop("ClientAmount")
-        .withColumnRenamed("Currency", "OriginalCurrency")
-        .withColumn("NumberOfEmployees", col("NumberOfEmployees").cast(IntegerType()))
-        .withColumn("OnboardingDate", col("ClientSince").cast(DateType()))
-        .drop("ClientSince")
-        .withColumn("LoanCheck", substring(col("HasLoan"), 1, 1))
-        .withColumn("Country", lit(""))
-        # .withColumn("Country", lit(None).cast(StringType()))
-        .withColumn("DiscountCheck", lit(""))
-        # .withColumn("DiscountCheck", lit(None).cast(StringType()))
-        .withColumn("SnapshotDate", col("SnapshotDate").cast(DateType()))
+    """
+    Maps and transforms columns from Source2 data to a standardized schema.
+
+    This function performs similar transformations as map_source1, but with the following differences:
+    * Extracts the first character of 'HasLoan' into 'LoanCheck'.
+    * Creates empty columns for 'Country' and 'DiscountCheck'.
+    * The original column names are already compatible with the standard schema.
+
+    Args:
+        df: A Spark DataFrame representing data from Source2.
+
+    Returns:
+        A transformed Spark DataFrame with a standardized schema.
+    """
+
+    return (
+        df.withColumn(
+            "ClientNumber", col("ClientNumber").cast(IntegerType())
+        )  # Cast to Integer
+        .withColumn(
+            "OriginalAmount", col("ClientAmount").cast(DoubleType())
+        )  # Cast to Double
+        .drop("ClientAmount")  # Drop original column after conversion
+        .withColumnRenamed("Currency", "OriginalCurrency")  # Rename column
+        .withColumn(
+            "NumberOfEmployees", col("NumberOfEmployees").cast(IntegerType())
+        )  # Cast to Integer
+        .withColumn(
+            "OnboardingDate", col("ClientSince").cast(DateType())
+        )  # Cast to Date
+        .drop("ClientSince")  # Drop original column after conversion
+        .withColumn(
+            "LoanCheck", substring(col("HasLoan"), 1, 1)
+        )  # Extract first character from 'HasLoan'
+        .withColumn("Country", lit(""))  # Create an empty 'Country' column
+        .withColumn("DiscountCheck", lit(""))  # Create an empty 'DiscountCheck' column
+        .withColumn(
+            "SnapshotDate", col("SnapshotDate").cast(DateType())
+        )  # Cast to Date
     ).select(
         "SourceSystem",
         "ClientGroup",
@@ -109,8 +161,7 @@ def map_source2(df: DataFrame):
         "Country",
         "DiscountCheck",
         "SnapshotDate",
-    )
-    return df
+    )  # Select and reorder columns
 
 
 def convert_to_euros(df, amount_col, currency_col, exchange_rates_df):
@@ -153,6 +204,25 @@ def convert_to_euros(df, amount_col, currency_col, exchange_rates_df):
 
 
 def is_client_secured(df, client_id_col, string_col, client_secured_df):
+    """
+    Determines whether clients are secured based on a lookup table.
+
+    This function joins a client DataFrame with a client secured indicator lookup table, and returns the
+    client DataFrame with an additional column "ClientSecuredIND" indicating whether the client is secured (True/False)
+    or not found in the lookup table (None).
+
+    Args:
+        df: A Spark DataFrame containing client data, including the client_id_col.
+        client_id_col: The name of the column in both DataFrames used for joining (e.g., "ClientNumber").
+        string_col: The name of the column in the client_secured_df containing the secured status indicator (e.g., "ClientSecuredInd").
+        client_secured_df: A Spark DataFrame with client_id_col and string_col to indicate if a client is secured ('Y'/'N').
+
+    Returns:
+        A Spark DataFrame containing the original client data and a new boolean column "ClientSecuredIND"
+        representing the client's secured status.
+    """
+
+    # Convert 'Y' and 'N' values in the lookup table to True/False for easier joining and subsequent analysis.
     client_secured_df = client_secured_df.withColumn(
         string_col,
         when(col(string_col) == "Y", True)
@@ -160,19 +230,32 @@ def is_client_secured(df, client_id_col, string_col, client_secured_df):
         .otherwise(None),  # Handle values other than 'Y' or 'N' as null
     )
 
+    # Join the client DataFrame with the lookup table based on the common client ID column.
+    # A left join ensures that all clients from the original DataFrame are kept, even if not found in the lookup.
     joined_df = df.join(
         client_secured_df,
         on=client_secured_df[client_id_col] == df[client_id_col],
-        how="left",  # Left join to keep all rows from the original DataFrame
+        how="left",
     )
 
+    # Rename the joined column to "ClientSecuredIND" for consistency and clarity.
     joined_df = joined_df.withColumnRenamed("ClientSecuredInd", "ClientSecuredIND")
 
+    # Select and return only the original client columns along with the new "ClientSecuredIND" column.
     return joined_df.select(df["*"], "ClientSecuredIND")
 
 
 def determine_enterprize_size(df):
-    df = df.withColumn(
+    """
+    Categorizes businesses into size classes ('S', 'M', 'L') based on their number of employees.
+
+    Args:
+        df: A Spark DataFrame with a column named 'NumberOfEmployees'.
+
+    Returns:
+        The input DataFrame with a new column 'EnterprizeSize' representing the size classification.
+    """
+    return df.withColumn(
         "EnterprizeSize",
         when(col("NumberOfEmployees") < 100, "S")
         .when(
@@ -181,27 +264,30 @@ def determine_enterprize_size(df):
         )
         .otherwise("L"),
     )
-    return df
 
 
 def calculate_secured_amount(df):
-    # create window for running totals
+    """
+    Calculates the 'SecuredAmount' for each client, based on a cumulative limit of 500,000 per client group,
+    and considering whether the client is secured ('ClientSecuredIND' = True).
+
+    Args:
+        df: A Spark DataFrame with columns 'ClientGroup', 'ClientSecuredIND', 'AmountEUR', and 'ClientNumber'.
+
+    Returns:
+        The input DataFrame with an additional column 'SecuredAmount' representing the calculated amount.
+    """
+    # Create window for calculating running totals of 'AmountEUR' per client group and secured status
     cumulative_window = (
         Window.partitionBy("ClientGroup", "ClientSecuredIND")
-        .orderBy(
-            "ClientNumber"
-            # in this case we will use rangeBetween for the sum
-        )
-        .rangeBetween(
-            # In this case we need to use Window.unboundedPreceding to catch all earlier rows
-            Window.unboundedPreceding,
-            0,
-        )
+        .orderBy("ClientNumber")  # Order within each partition
+        .rangeBetween(Window.unboundedPreceding, 0)  # Range for cumulative sum
     )
     normal_window = Window.partitionBy("ClientGroup", "ClientSecuredIND").orderBy(
         "ClientNumber"
     )
 
+    # Calculate the remaining secured amount for each row within the group
     df_cumulative = df.withColumn(
         "remaining_amount",
         when(
@@ -209,27 +295,28 @@ def calculate_secured_amount(df):
         ),
     )
 
+    # Calculate the 'SecuredAmount' for each row based on various conditions
     df_allocated = df_cumulative.withColumn(
         "SecuredAmount",
         when(
             (lag("remaining_amount", 1).over(normal_window).isNull())
             & (
                 col("remaining_amount") >= 0
-            ),  # Check if previous row does not exist and current row is positive
+            ),  # First row in group, enough remaining amount
             col("AmountEUR"),
         )
         .when(
             (lag("remaining_amount", 1).over(normal_window).isNull())
             & (
                 col("remaining_amount") < 0
-            ),  # Check if previous row does not exist and current row is negative
+            ),  # First row in group, not enough remaining amount
             lit(500000),
         )
         .when(
             (lag("remaining_amount", 1).over(normal_window).isNotNull())
             & (
                 col("remaining_amount") >= 0
-            ),  # Check if previous row exists and current row is positive
+            ),  # Not the first row, enough remaining amount
             col("AmountEUR"),
         )
         .when(
@@ -237,16 +324,25 @@ def calculate_secured_amount(df):
             & (col("remaining_amount") < 0)
             & (
                 lag("remaining_amount", 1).over(normal_window) > 0
-            ),  # Check if previous row does not exist and current row is positive
+            ),  # Not the first row, not enough remaining, but previous had enough
             lag("remaining_amount", 1).over(normal_window),
         )
-        .otherwise(lit(0)),
-    )
-    df_allocated = df_allocated.drop("remaining_amount")
+        .otherwise(lit(0)),  # Default value for other cases
+    ).drop(
+        "remaining_amount"
+    )  # Remove the temporary 'remaining_amount' column
     return df_allocated
 
 
 def main():
+    """
+    Main data processing pipeline:
+    1. Loads data from 'bronze' database.
+    2. Performs data cleaning and validation.
+    3. Transforms and enriches data.
+    4. Saves the final result to the 'silver' database and a CSV file.
+    """
+
     spark.sql("CREATE DATABASE IF NOT EXISTS silver")
     spark.sql("USE bronze")
 
